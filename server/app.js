0390e805829d109676827ca861a70f34d34600f6
Peter Gao <mercenary90@gmail.com>
2013-09-01 12:19:27 -0700

    checkpoint before attempting to do teletype with just javascript

--- server/app.js
+++ server/app.js
@@ -3,9 +3,8 @@ var handlers = require('./handlers');
 
 var app = express();
 
-// app.use(express.compress());
+/*app.use(express.compress());*/
 app.use(express.static('./static'));
-// app.get('/', handlers.home);
 app.get('/random', handlers.random);
 
 var port = process.env.PORT || 8888;
